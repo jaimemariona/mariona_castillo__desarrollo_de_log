e6cdba43920ed66a4482725208e6684ff1726d78
Jaime Alcides Mariona Castillo <jamc789780@gmail.com>
2025-05-18 16:21:45 -0600

    Actualización de archivos y adición de nuevos

--- core/secure/ips.php
+++ core/secure/ips.php
@@ -1,8 +1,9 @@
 <?php
 $rango = array(
-    "127.0.0.1/32", //PERMITE UNICAMENTE LA DIRECCION LOCALHOST
-    "192.168.1.0/24", //PERMITE TODOS LOS HOST DE LA RED 192.168.1.*
-    "10.0.0.*", //PERMITE TODOS LOS HOST DE LA RED 10.0.0.X
-    "172.16.10.1-172.16.10.50" //PERMITE EL RANGO DE IP ESPECIFICO.
+    "127.0.0.1/32",  // IPv4 para localhost
+    "::1/128",       // IPv6 para localhost
+    "192.168.1.0/24", 
+    "10.0.0.0/8", 
+    "172.16.10.0/24" 
 );
 ?>
